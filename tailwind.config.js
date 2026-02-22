d7c1eff6cd5f1bc02d8cabc38aac03eb2ed5ac2b
bhawna kumari <bhawnakumari1214@gmail.com>
2026-02-22 13:49:49 +0530

    update tailwind imports  global.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,3 @@
 module.exports = {
   darkMode: "selector",
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
 };
